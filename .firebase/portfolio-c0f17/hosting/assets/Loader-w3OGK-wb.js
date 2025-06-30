fd7a5808e2740a0a483ac73c731dbd57428ab17f
khush <khushramnani@gmail.com>
2025-06-30 10:22:57 +0530

    Update portfolio assets and enhance scrollbar styling
    - Updated script and stylesheet references in index.html for the portfolio.
    - Refined scrollbar styles in index.css for better aesthetics and cross-browser compatibility.

--- .firebase/portfolio-c0f17/hosting/assets/Loader-w3OGK-wb.js
+++ .firebase/portfolio-c0f17/hosting/assets/Loader-w3OGK-wb.js
@@ -1,4 +1,4 @@
-import{c as f,r as a,u as c,j as e,g as o}from"./index-Dalz46C7.js";/**
+import{c as f,r as a,u as c,j as e,g as o}from"./index-oNSGr6uc.js";/**
  * @license lucide-react v0.523.0 - ISC
  *
  * This source code is licensed under the ISC license.
